45d58baf1ec8d98cca74af812ed05bf358c9335a
jku20 <jku-ben20@stuy.edu>
2020-11-24 23:52:24 -0500

    added command line functionality

--- main.c
+++ main.c
@@ -13,13 +13,25 @@ void safe_open(char *d, DIR **df) {
     }
 }
 
-int file_size(struct dirent *de) {
+int file_size(struct dirent *de, char *cwd) {
+    //introduces a hard limit of 511 to path length but that should be fine
+    char path[512];
     if(0) {//de->d_type == DT_DIR) { currently like this untill I do (or don't do) recursive thing
 
     } else {
+        int a = 0;
+        while(a < 512 && cwd[a]) {
+            path[a++] = cwd[a];
+        }
+        path[a++] = '/';
+        int b = 0;
+        while(a < 512 && de->d_name[b]) {
+            path[a++] = de->d_name[b++];
+        }
+        path[a] = '\0';
         struct stat s;
-        if(stat(de->d_name,&s) == -1) {
-            printf("error %d: %s\n", errno, strerror(errno));
+        if(stat(path,&s) == -1) {
+            printf("using path %s, error %d: %s\n",path, errno, strerror(errno));
             exit(-1);
         }
         return s.st_size;
@@ -33,11 +45,21 @@ void print_file_info(struct dirent *de) {
         printf("Regular File %s\n",de->d_name);
     }
 }
-int main() {
-    printf("Statistics For Directory: .\n\n"); //am I missing something because how would the program observe any other directory as it is looking at the current directory which is always .
+
+int main(int argc, char **argv) {
+    char to_analyze[255];
+    if(argc <= 1) {
+        printf("Enter a Directory:");
+        fgets(to_analyze, sizeof(to_analyze), stdin);
+        *strchr(to_analyze,'\n') = '\0';
+    } else {
+        strncpy(to_analyze, argv[1], sizeof(to_analyze)-1);
+    }
+
+    printf("Statistics For Directory: %s\n\n", to_analyze); 
 
     DIR *df;
-    safe_open(".", &df);
+    safe_open(to_analyze, &df);
 
     struct dirent *cur = readdir(df);
     long long total_size = 0;
@@ -57,7 +79,7 @@ int main() {
             cur = readdir(df);
             continue;
         }
-        total_size += file_size(cur);
+        total_size += file_size(cur,to_analyze);
         print_file_info(cur);
         cur = readdir(df);
     }
