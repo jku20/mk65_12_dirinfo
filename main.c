0396abb31e08ca5a0d5b4cb568f5c5441b0487c3
jku20 <jku-ben20@stuy.edu>
2020-11-25 14:35:09 -0500

    pretty sure it now goes recursively into directories though haven't rigoursly tested

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+//DOES NOT WORK YET, GOES INTO INFINATE LOOP
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
@@ -13,23 +14,36 @@ void safe_open(char *d, DIR **df) {
     }
 }
 
-int file_size(struct dirent *de, char *cwd) {
+long long file_size(struct dirent *de, char *cwd) {
     //introduces a hard limit of 511 to path length but that should be fine
+    if(!strcmp(de->d_name,"..") || !strcmp(de->d_name,".")) {
+        return 0;
+    }
     char path[512];
-    if(0) {//de->d_type == DT_DIR) { currently like this untill I do (or don't do) recursive thing
+    int a = 0;
+    while(a < 512 && cwd[a]) {
+        path[a++] = cwd[a];
+    }
+    path[a++] = '/';
+    int b = 0;
+    while(a < 512 && de->d_name[b]) {
+        path[a++] = de->d_name[b++];
+    }
+    path[a] = '\0';
 
+    if(de->d_type == DT_DIR) { //currently like this untill I do (or don't do) recursive thing
+        long long tot_from_dir = 0;
+        DIR* df;
+        safe_open(path, &df);
+        struct dirent *cur = readdir(df);
+        while(cur) {
+            tot_from_dir += file_size(cur, path);
+            cur = readdir(df);
+        }
+        closedir(df);
+        return tot_from_dir;
     } else {
-        int a = 0;
-        while(a < 512 && cwd[a]) {
-            path[a++] = cwd[a];
-        }
-        path[a++] = '/';
-        int b = 0;
-        while(a < 512 && de->d_name[b]) {
-            path[a++] = de->d_name[b++];
-        }
-        path[a] = '\0';
-        struct stat s;
+                struct stat s;
         if(stat(path,&s) == -1) {
             printf("using path %s, error %d: %s\n",path, errno, strerror(errno));
             exit(-1);
@@ -68,7 +82,7 @@ int main(int argc, char **argv) {
             cur = readdir(df);
             continue;
         }
-        //total_size += file_size(cur); //not including directories for now
+        total_size += file_size(cur,to_analyze); //not including directories for now
         print_file_info(cur);
         cur = readdir(df);
     }
